75e5764aabe00ac1b3bf454bc576d5be5c4dcab8
razi16 <arazi916@gmail.com>
2023-03-25 13:56:06 +0700

    Updating 'src' tags

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,64 +1,100 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import PageTittle from '../components/PageTittle'
+import React from "react";
+import { Link } from "react-router-dom";
+import PageTittle from "../components/PageTittle";
 /* import Date from '../../public/icons/date.png' */
 
 function About(props) {
   return (
-    <main className='main-about' onClick={props.handleClick}>
-      <div className='animated'>
-      <PageTittle title={'About'}/>
-        <section className='about-section'>
-            <p>My name is Ahmad Razi, a student majoring in Information Systems from National Development University "Veteran" Jakarta. 
-            I have skills in front-end web development. My strengths are that I can work individually or in a group and can manage my time well. 
-            With my skills, I believe I can build a user-friendly web application with wide accessibility. 
-            In addition, I can also design UI/UX and design application information systems.</p>
+    <main className="main-about" onClick={props.handleClick}>
+      <div className="animated">
+        <PageTittle title={"About"} />
+        <section className="about-section">
+          <p>
+            My name is Ahmad Razi, a student majoring in Information Systems
+            from National Development University "Veteran" Jakarta. I have
+            skills in front-end and back end web development. My strengths are
+            that I can work individually or in a group and can manage my time
+            well. With my skills, I believe I can build a user-friendly web
+            application with wide accessibility and i can build a backend for
+            web application such as user authentication, creating and using Rest
+            API and implementing CRUD. In addition, I can also design UI/UX and
+            design application information systems.
+          </p>
         </section>
-        <section className='my-details'>
-        <ul>
-          <li><b>Date of birth:</b> 16 September 2001</li>
-          <li><b>Website:</b> www.domain.com</li>
-          <li><b>Country:</b> Indonesia</li>
-          <li><b>City:</b> Jakarta</li>
-      {/*   </ul>
+        <section className="my-details">
+          <ul>
+            <li>
+              <b>Date of birth:</b> 16 September 2001
+            </li>
+            <li>
+              <b>Website:</b> www.domain.com
+            </li>
+            <li>
+              <b>Country:</b> Indonesia
+            </li>
+            <li>
+              <b>City:</b> Jakarta
+            </li>
+            {/*   </ul>
         <ul > */}
-          <li><b>Age:</b> 21</li>
-          <li><b>Education:</b> National Development University "Veteran" Jakarta (2019 - now)</li>
-          <li><b>Internship/Work:</b> Available</li>
-        </ul>
+            <li>
+              <b>Age:</b> 21
+            </li>
+            <li>
+              <b>Education:</b> National Development University "Veteran"
+              Jakarta (2019 - now)
+            </li>
+            <li>
+              <b>Internship/Work:</b> Available
+            </li>
+          </ul>
         </section>
 
-        <Link to="/downloadables/CV-Ahmad-Razi.pdf" target="_blank" download><button className='download-button'>Download my CV</button></Link>
+        <Link to="/downloadables/CV-Ahmad-Razi.pdf" target="_blank" download>
+          <button className="download-button">Download my CV</button>
+        </Link>
 
-        <section className='experience-and-certificate'>
-        <div className='experience'>
-          <h2>Experience</h2>
-          <div className='card'>
-          <img src='/icons/date.png' alt='date' style={{display: 'inline'}} width='20px' height='20px'/>
-          <p style={{display: 'inline', marginLeft: '5px'}}>February - July 2022</p>
-          <h3>Independent Campus</h3>
-          <p>An activity that aims to encourage students to master various sciences that are useful for entering the world of work. 
-          In this activity I conducted independent study activities in the field of front-end and back-end web development at Dicoding 
-          Indonesia.</p>
+        <section className="experience-and-certificate">
+          <div className="experience">
+            <h2>Experience</h2>
+            <div className="card">
+              <img
+                src="/icons/date.png"
+                alt="date"
+                style={{ display: "inline" }}
+                width="20px"
+                height="20px"
+              />
+              <p style={{ display: "inline", marginLeft: "5px" }}>
+                February - July 2022
+              </p>
+              <h3>Independent Campus</h3>
+              <p>
+                An activity that aims to encourage students to master various
+                sciences that are useful for entering the world of work. In this
+                activity I conducted independent study activities in the field
+                of front-end and back-end web development at Dicoding Indonesia.
+              </p>
+            </div>
           </div>
-        </div>
 
-        <div className='certificate'>
-          <h2>Certificate</h2>
-          <div className='card'>
-          <h3 style={{paddingRight: '20px'}}>Certificate of Completion of MSIB Batch 2 Dicoding</h3>
-          <p>Have successfully completed the Front-End Web and Back-End Training at the Independent Study Program Certified 
-          Independent Campus Batch 2 2022 with Dicoding Indonesia</p>
+          <div className="certificate">
+            <h2>Certificate</h2>
+            <div className="card">
+              <h3 style={{ paddingRight: "20px" }}>
+                Certificate of Completion of MSIB Batch 2 Dicoding
+              </h3>
+              <p>
+                Have successfully completed the Front-End Web and Back-End
+                Training at the Independent Study Program Certified Independent
+                Campus Batch 2 2022 with Dicoding Indonesia
+              </p>
+            </div>
           </div>
-        </div>
-        
-        
         </section>
       </div>
-        
-        
     </main>
-  )
+  );
 }
 
-export default About
+export default About;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,31 +1,35 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Home(props) {
   return (
     <main onClick={props.handleClick}>
-      <div className='home-container animated'>
-
-      
-        <section className='home-section'>
-        <h1 className='home-header' style={{marginTop: '15%'}}>Hello, my name is <span className='orange'>Ahmad Razi</span></h1>
-        <h1 className='home-h1'>I'm a <span className='orange'>front end developer</span></h1>
-        <p className='home-p'>I'm a student at the National Development University "Veteran" Jakarta majoring in information systems. My
-        expertise is to create an appealing website which is responsible and accessible. I'm currently looking for an opportunity to do an 
-        internship as a front end developer</p>
-        <Link to='/about'><button className='home-button'>More about me</button></Link>
-      </section >
-       <div className='partial-border'>
-        <img className='home-img' src='./images/maimage.jpg' alt='Ahmad Razi' />
-       </div>
- </div>
-      
-        
-      
-        
+      <div className="home-container animated">
+        <section className="home-section">
+          <h1 className="home-header" style={{ marginTop: "15%" }}>
+            Hello, my name is <span className="orange">Ahmad Razi</span>
+          </h1>
+          <h1 className="home-h1">
+            I'm a <span className="orange">fullstack web developer</span>
+          </h1>
+          <p className="home-p">
+            I'm a student at the National Development University "Veteran"
+            Jakarta majoring in information systems. My expertise is to create
+            an appealing website which is responsible and accessible and
+            creating a backend for a web application. I'm currently looking for
+            an opportunity to do an internship as a front end, back end or
+            fullstack web developer.
+          </p>
+          <Link to="/about">
+            <button className="home-button">More about me</button>
+          </Link>
+        </section>
+        <div className="partial-border">
+          <img className="home-img" src="maimage.jpg" alt="Ahmad Razi" />
+        </div>
+      </div>
     </main>
-  )
+  );
 }
 
-export default Home
+export default Home;
